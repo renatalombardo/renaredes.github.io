e77dc5f691d7078265c0a9ec581be092740924c4
renatalombardo <olga.lombardo@comunidad.ub.edu.ar>
2025-09-15 16:26:22 -0300

    json

--- Especiales/01-TablasJson/json.js
+++ Especiales/01-TablasJson/json.js
@@ -1,13 +1,6 @@
 var textoJSONPedidos = '{' +
-  '"UnidadesMedida":[' +
-    '{"codUM":"UM01","Descripcion":"Bolsa 1 kg"},' +
-    '{"codUM":"UM02","Descripcion":"Bolsa 10 kg"},' +
-    '{"codUM":"UM03","Descripcion":"Caja 1 lt"},' +
-    '{"codUM":"UM04","Descripcion":"Botella 2 lt"},' +
-    '{"codUM":"UM05","Descripcion":"Pack 6 unidades"}' +
-  '],' +
-
-  '"RenglonesPedido":[' +
+  '"RenglonesPedido":' + 
+  '[' +
     '{"NroDePedido":"001","Cod_articulo":"A100","Descripcion":"Arroz Premium","UnidadMedida":"UM01","Cantidad":12,"PrecioUnitario":750.50,"Pdf_comprobante":"arroz001.pdf"},' +
     '{"NroDePedido":"002","Cod_articulo":"A101","Descripcion":"Fideos Spaghetti","UnidadMedida":"UM01","Cantidad":20,"PrecioUnitario":680.00,"Pdf_comprobante":"fideos002.pdf"},' +
     '{"NroDePedido":"003","Cod_articulo":"A102","Descripcion":"Harina 000","UnidadMedida":"UM01","Cantidad":25,"PrecioUnitario":430.75,"Pdf_comprobante":"harina003.pdf"},' +
@@ -23,45 +16,14 @@ var textoJSONPedidos = '{' +
     '{"NroDePedido":"013","Cod_articulo":"A112","Descripcion":"Vino Tinto Malbec","UnidadMedida":"UM04","Cantidad":30,"PrecioUnitario":2500.00,"Pdf_comprobante":"vino013.pdf"},' +
     '{"NroDePedido":"014","Cod_articulo":"A113","Descripcion":"Cerveza Rubia","UnidadMedida":"UM05","Cantidad":24,"PrecioUnitario":1350.40,"Pdf_comprobante":"cerveza014.pdf"},' +
     '{"NroDePedido":"015","Cod_articulo":"A114","Descripcion":"Gaseosa Cola","UnidadMedida":"UM05","Cantidad":18,"PrecioUnitario":1150.00,"Pdf_comprobante":"gaseosa015.pdf"}' +
+  '],' +
+
+  '"UnidadesMedida":' + 
+  '[' +
+    '{"codUM":"UM01","Descripcion":"Bolsa 1kg"},' +
+    '{"codUM":"UM02","Descripcion":"Bolsa 10kg"},' +
+    '{"codUM":"UM03","Descripcion":"Caja 1lt"},' +
+    '{"codUM":"UM04","Descripcion":"Botella 2lt"},' +
+    '{"codUM":"UM05","Descripcion":"Pack 6 uds"}' +
   ']' +
 '}';
-
-(function() {
-  const datos = JSON.parse(textoJSONPedidos);
-
-  var tablaPedidosBody = document.getElementById('pedidos-body');
-  var tablaUnidadesBody = document.getElementById('unidades-body');
-  var botonUnidades = document.getElementById('boton-unidades');
-  var contenedorUnidades = document.getElementById('tabla-unidades-contenedor');
-
-  botonUnidades.addEventListener('click', function() {
-    if (contenedorUnidades.style.display === "block") {
-      contenedorUnidades.style.display = "none";
-    } else {
-      contenedorUnidades.style.display = "block";
-    }
-  });
-
-  for (var i = 0; i < datos.RenglonesPedido.length; i++) {
-    var pedido = datos.RenglonesPedido[i];
-    var fila = document.createElement('tr');
-    fila.innerHTML = '<td>' + pedido.NroDePedido + '</td>' +
-                     '<td>' + pedido.Cod_articulo + '</td>' +
-                     '<td>' + pedido.Descripcion + '</td>' +
-                     '<td>' + pedido.UnidadMedida + '</td>' +
-                     '<td>' + pedido.Cantidad + '</td>' +
-                     '<td>' + pedido.PrecioUnitario.toFixed(2) + '</td>' +
-                     '<td><a href="#">' + pedido.Pdf_comprobante + '</a></td>';
-                
-    tablaPedidosBody.appendChild(fila);
-  }
-
-  for (var i = 0; i < datos.UnidadesMedida.length; i++) {
-    var unidad = datos.UnidadesMedida[i];
-    var fila = document.createElement('tr');
-    fila.innerHTML = '<td>' + unidad.codUM + '</td>' +
-                     '<td>' + unidad.Descripcion + '</td>';
-                
-    tablaUnidadesBody.appendChild(fila);
-  }
-})();
